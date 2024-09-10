34d3661684c1496324e02474c4bfe5106238ffb5
João Barreto <joaobarretoprof@gmail.com>
2024-09-10 11:27:54 -0300

    fix: configure server and update routes

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const carsRoutes = require("./routes/cars.js");
+const carItemsRoutes = require("./routes/carItems.js");
+
 const app = express();
 
-const port = 4000;
+app.use(bodyParser.json());
+
+app.use("/api/cars", carsRoutes);
+app.use("/api/carItems", carItemsRoutes);
+
+const port = process.env.port || 4000;
 
 app.listen(port, (error) => {
   if (error) {
--- routes/carItems.js
+++ routes/carItems.js
@@ -1,11 +1,11 @@
 const express = require("express");
-const router = express.router;
-const carsController = require("../controllers/carItemsController.js");
+const router = express.Router();
+const carItemsController = require("../controllers/carItemsController.js");
 
 router.post("/", carItemsController.create);
-router.get("/", carsController.findAll);
+router.get("/", carItemsController.findAll);
 router.get("/car/:car_id", carItemsController.findByCarId);
 router.put("/:id", carItemsController.update);
 router.delete("/:id", carItemsController.delete);
 
-module.exports - router;
+module.exports = router;
